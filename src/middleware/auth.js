da9eb2c5cbcc7b07ab22764272f8d08480090be4
Vlad Ilis <ilis_vlad1990@yahoo.com>
2019-09-27 10:48:51 +0300

    First build

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 
-// veryfy credentials
 const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
